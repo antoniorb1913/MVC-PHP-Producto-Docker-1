68d2057306b645e356d612801444bcf93001f4c7
Cipri <cipri.profesor.informatica@gmail.com>
2025-11-25 17:02:25 +0100

    Update configDB.php

--- config/configDB.php
+++ config/configDB.php
@@ -24,10 +24,31 @@ class configDB {
      }
 
      private function getValues(){
-        $conf =  parse_ini_file('config.ini');
-        self::$host = $conf['host'];
-        self::$user = $conf['user'];
-        self::$pass = $conf['pass'];
+        
+    // 1) Si estamos en Render, pues hay que usar variables de entorno
+      if (getenv('DB_HOST')) {
+
+         $host = getenv('DB_HOST');
+         $name = getenv('DB_NAME');
+         $user = getenv('DB_USER');
+         $pass = getenv('DB_PASSWORD');
+         $port = getenv('DB_PORT') ?: 3306;
+
+         // construimos el DSN igual que esta en config.ini
+         self::$host = "mysql:host={$host};port={$port};dbname={$name};charset=utf8mb4";
+         self::$user = $user;
+         self::$pass = $pass;
+
+      }
+      else{
+         // 2) En local,  leer config.ini como siempre
+         $conf = parse_ini_file('config.ini');
+
+         self::$host = $conf['host']; 
+         self::$user = $conf['user'];
+         self::$pass = $conf['pass'];
+      }
+
      }
 
      /**
